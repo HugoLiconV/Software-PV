fe4b3f0c6b7daf2aebe26493f6b71f61e52099ed
Hugo <hugo_val28@hotmail.com>
2017-12-04 18:07:40 -0700

    documentacion de ventas

--- src/api/index.js
+++ src/api/index.js
@@ -12,4 +12,19 @@ router.use('/suppliers', supplier);
 router.use('/register', register);
 router.use('/transaction', transaction);
 router.use('/sales', sale);
+
+/**
+ * @apiDefine user User access only
+ * You must pass `access_token` parameter or a Bearer Token authorization header
+ * to access this endpoint.
+ */
+/**
+ * @apiDefine listParams
+ * @apiParam {String} [q] Query to search.
+ * @apiParam {Number{1..30}} [page=1] Page number.
+ * @apiParam {Number{1..100}} [limit=30] Amount of returned items.
+ * @apiParam {String[]} [sort=-createdAt] Order of returned items.
+ * @apiParam {String[]} [fields] Fields to be returned.
+ */
+
 export default router;
--- src/api/ventas/index.js
+++ src/api/ventas/index.js
@@ -12,17 +12,64 @@ const router = express.Router()
 
 const {transactions, total} = schema.tree;
 
-// set the routes
+
+/**
+ * @api {post} /sales Crea una venta
+ * @apiName CreateSale
+ * @apiGroup User
+ * @apiParam [Transacciones] Array de las ids de las transacciones
+ * @apiParam Total total de la venta
+ * @apiSuccess {Object} sales Datos de la venta.
+ * @apiError {Object} 400 Parametros incorrectos o valores erroneos.
+ * @apiError 404 venta no encontrada.
+ */
 router.post('/',
 	body({transactions, total}),
 	create)
+
+/**
+ * @api {get} /sales Regresa las ventas.
+ * @apiName RetrieveSales
+ * @apiGroup User
+ * @apiUse listParams
+ * @apiSuccess {Object[]} sales Lista de ventas.
+ * @apiError {Object} 400 Parametros incorrectos o valores erroneos.
+ */
 router.get('/',
 	index)
+
+/**
+ * @api {get} /sales/:id Regresa una venta 
+ * @apiName RetrieveSale
+ * @apiGroup User
+ * @apiSuccess {Object} sa;e Datos de la venta
+ * @apiError {Object} 400 Parametros incorrectos o valores erroneos.
+ * @apiError 404 Venta no encontrada.
+ */
 router.get('/:id',
 	show)
+
+/**
+ * @api {put} /sales/:id Actualizar venta
+ * @apiName UpdateSale
+ * @apiGroup User
+ * @apiParam [Transacciones] Array de las ids de las transacciones
+ * @apiParam Total total de la venta
+ * @apiSuccess {Object} Sale datos de la venta.
+ * @apiError {Object} 400 Parametros incorrectos o valores erroneos.
+ * @apiError 404 Venta no encontrada.
+ */
 router.put('/:id',
 	body({transactions, total}),
 	update)
+
+/**
+ * @api {delete} /sales/:id Eliminar venta
+ * @apiName DeleteSale
+ * @apiGroup User
+ * @apiSuccess (Success 204) 204 Sin contenido.
+ * @apiError 404 Venta no encontrada.
+ */
 router.delete('/:id',
 	destroy)
 export default router;
